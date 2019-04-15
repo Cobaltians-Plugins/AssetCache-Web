505de5b5b151ade9d576f2eede86fb770f1e055c
Guillaume Gendre <dzb.rtz@gmail.com>
2019-04-15 10:39:43 +0200

    ready to handle event

--- cobalt.asset-cache.js
+++ cobalt.asset-cache.js
@@ -1,21 +1,34 @@
-(function(cobalt){
-    var plugin = {
-        classes: {
-			ios: "AssetCachePlugin",
-            android: "io.kristal.assetcache.AssetCache"
-        },
-        init: function () {
-            cobalt.assetCache={
-                download: this.download.bind(this),
-				delete: this.delete.bind(this)
-            };
-        },
-		download: function(params, callback){
-			cobalt.plugins.send(this, 'download', params, callback)
-		},
-		delete: function(params, callback){
-			cobalt.plugins.send(this, 'delete', params, callback)
-		}
-    };
-    cobalt.plugins.register(plugin);
+(function(cobalt) {
+  var plugin = {
+    classes: {
+      ios: "AssetCachePlugin",
+      android: "io.kristal.assetcache.AssetCache"
+    },
+    init: function() {
+      cobalt.assetCache = {
+        download: this.download.bind(this),
+        delete: this.delete.bind(this)
+      };
+    },
+    callbacks: {},
+    randomId : function(){
+      return Math.random().toString(36).substr(2, 10);
+    },
+    download: function(params, callback) {
+      params.callback = this.randomId();
+      this.callbacks[params.callback] = callback;
+      cobalt.plugins.send(this, 'download', params)
+    },
+    delete: function(params, callback) {
+      params.callback = this.randomId();
+      this.callbacks[params.callback] = callback;
+      cobalt.plugins.send(this, 'delete', params)
+    },
+    handleEvent: function(json) {
+      if (json.callback && typeof this.callbacks[json.callback] === 'function') {
+        this.callbacks[json.callback](json.data);
+      }
+    }
+  };
+  cobalt.plugins.register(plugin);
 })(cobalt || {});
