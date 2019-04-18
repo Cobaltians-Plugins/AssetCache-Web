1556586b4ed7a34f3bcbba5214ad3c1e53f992d1
Guillaume Gendre <dzb.rtz@gmail.com>
2019-04-18 15:27:35 +0200

    using cobalt.plugins.send with callback

--- cobalt.asset-cache.js
+++ cobalt.asset-cache.js
@@ -11,24 +11,11 @@
         delete: this.delete.bind(this)
       };
     },
-    callbacks: {},
-    randomId : function(){
-      return Math.random().toString(36).substr(2, 10);
-    },
     download: function(params, callback) {
-      params.callback = this.randomId();
-      this.callbacks[params.callback] = callback;
-      cobalt.plugins.send(this, 'download', params)
+      cobalt.plugins.send(this, 'download', params, callback)
     },
     delete: function(params, callback) {
-      params.callback = this.randomId();
-      this.callbacks[params.callback] = callback;
-      cobalt.plugins.send(this, 'delete', params)
-    },
-    handleEvent: function(json) {
-      if (json.data & json.data.callback && typeof this.callbacks[json.data.callback] === 'function') {
-        this.callbacks[json.data.callback](json.data);
-      }
+      cobalt.plugins.send(this, 'delete', params, callback)
     }
   };
   cobalt.plugins.register(plugin);
